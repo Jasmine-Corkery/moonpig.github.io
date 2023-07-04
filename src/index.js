990c0f70e397641781ddb56d063785b912cc8676
Jasmine-Corkery <jasmine.corkery10573@gmail.com>
2023-07-04 23:59:40 +0100

    Add built files for deployment

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// creates a root and specifies the root element in order to render the app
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// renders the app within the root element
-root.render(
+// renders the app component in the root element of the DOM
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 reportWebVitals();
